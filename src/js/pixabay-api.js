4d92ea3c73fe47515d807d99bfc13de0f02eb10a
lesia_onyshchuk <lkane443@gmail.com>
2024-12-07 03:58:59 +0200

    css js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,88 +1,22 @@
-import markup from '../js/render-functions.js';
-
-import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-import { hideLoader } from '../main.js';
 
 import axios from 'axios';
 
-let lightbox = new SimpleLightbox('.gallery .gallery-link', {
-  captions: true,
-  captionsData: 'alt',
-  captionDelay: 250,
-});
-
 const API_KEY = '47418374-a80c993bb5abb784419bb47e5';
 
-let page = 2;
-let perPage = 15;
-
-const gallery = document.querySelector('.gallery');
-const loadBtn = document.querySelector('.load-more');
-
-export const fetchFoo = async info => {
+export const fetchFoo = async (info, page = 1) => {
   const params = new URLSearchParams({
     key: API_KEY,
     q: info,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-    _limit: perPage,
-    _page: page,
+    per_page: 200,
+    page: page,
   });
 
-  axios
-    .get(`https://pixabay.com/api/?key=${API_KEY}&q=${params}`)
-    .then(data => {
-      console.log(data);
-      if (data.data.hits.length === 0) {
-        iziToast.error({
-          title: 'Error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          backgroundColor: '#ef4040',
-          messageColor: '#ffffff',
-          messageSize: '16px',
-          titleColor: '#ffffff',
-        });
-      }
-      gallery.innerHTML = '';
-      console.log(data);
-
-      gallery.insertAdjacentHTML('beforeend', markup(data.data.hits));
-      lightbox.refresh();
-    })
-    .catch(error => {
-      iziToast.error({
-        title: 'Error',
-        message: `An error occurred: ${error.message}`,
-        position: 'topRight',
-        backgroundColor: '#ef4040',
-        messageColor: '#ffffff',
-        messageSize: '16px',
-        titleColor: '#ffffff',
-      });
-    })
-    .finally(() => {
-      hideLoader();
-    });
+  const response = await axios.get(
+    `https://pixabay.com/api/?key=${API_KEY}&q=${params}`
+  );
+  return response.data;
 };
-
-loadBtn.addEventListener('click', async () => {
-  try {
-    const posts = await fetchFoo(value);
-    markup(posts);
-    page += 1;
-
-    if (page > 1) {
-      loadBtn.textContent = 'Load more';
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './css/loader.css';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import markup from './js/render-functions.js';
+
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
@@ -7,6 +12,16 @@ import { fetchFoo } from './js/pixabay-api.js';
 
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
+
+let lightbox = new SimpleLightbox('.gallery .gallery-link', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+loadBtn.setAttribute('hidden', true);
+loadBtn.style.display = 'none';
 
 function showLoader() {
   loader.style.display = 'block';
@@ -16,12 +31,25 @@ export function hideLoader() {
   loader.style.display = 'none';
 }
 
+let value = '';
+let currentPage = 1;
+let perPage = 200;
+
+document.addEventListener('DOMContentLoaded', reloadFoo);
+
+function reloadFoo() {
+  loadBtn.hidden = true;
+  loadBtn.style.display = 'none';
+}
+
 if (!form.dataset.listenerAdded) {
   form.addEventListener('submit', event => {
     event.preventDefault();
 
+    loadBtn.setAttribute('hidden', true);
+
     const input = event.target.elements.search;
-    const value = input.value.trim();
+    value = input.value.trim();
 
     showLoader();
 
@@ -41,11 +69,76 @@ if (!form.dataset.listenerAdded) {
       return;
     } else {
       showLoader();
-      fetchFoo(value);
+      fetchFoo(value)
+        .then(data => {
+          if (data.hits.length < perPage) {
+            loadBtn.setAttribute('hidden', true);
+          }
+          if (data.hits.length === 0) {
+            loadBtn.setAttribute('hidden', true);
+            iziToast.error({
+              title: 'Error',
+              message:
+                'Sorry, there are no images matching your search query. Please try again!',
+              position: 'topRight',
+              backgroundColor: '#ef4040',
+              messageColor: '#ffffff',
+              messageSize: '16px',
+              titleColor: '#ffffff',
+            });
+          }
+          gallery.innerHTML = '';
+
+          gallery.insertAdjacentHTML('beforeend', markup(data.hits));
+
+          lightbox.refresh();
+        })
+        .catch(error => {
+          iziToast.error({
+            title: 'Error',
+            message: `An error occurred: ${error.message}`,
+            position: 'topRight',
+            backgroundColor: '#ef4040',
+            messageColor: '#ffffff',
+            messageSize: '16px',
+            titleColor: '#ffffff',
+          });
+        })
+        .finally(() => {
+          hideLoader();
+        });
+
+      loadBtn.removeAttribute('hidden');
     }
 
     form.reset();
+    return value;
   });
 
   form.dataset.listenerAdded = true;
 }
+
+loadBtn.addEventListener('click', async () => {
+  currentPage += 1;
+
+  try {
+    const posts = await fetchFoo(value, currentPage);
+    const totalPages = Math.ceil(posts.totalHits / perPage);
+    gallery.insertAdjacentHTML('beforeend', markup(posts.hits));
+
+    if (currentPage >= totalPages) {
+      loadBtn.hidden = true;
+      return iziToast.error({
+        title: 'Warning',
+        message: "We're sorry, there are no more posts to load",
+        position: 'topRight',
+        backgroundColor: '#f39c12',
+        messageColor: '#ffffff',
+        messageSize: '16px',
+        titleColor: '#ffffff',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
